3f98ab71b3f219a9e30bb4f2a3a649c87be3a7ab
B. van Berkum <dev@dotmpe.com>
2017-10-01 20:56:12 +0200

    rtd conf

--- conf.py
+++ conf.py
@@ -7,5 +7,16 @@ source_parsers = {
   '.md': CommonMarkParser,
 }
 
-print dir(globals())
+print globals().keys()
+"""
+['__class__', '__cmp__', '__contains__', '__delattr__', '__delitem__',
+'__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__getitem__',
+'__gt__', '__hash__', '__init__', '__iter__', '__le__', '__len__', '__lt__',
+'__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__',
+'__setitem__', '__sizeof__', '__str__', '__subclasshook__', 'clear', 'copy',
+'fromkeys', 'get', 'has_key', 'items', 'iteritems', 'iterkeys', 'itervalues',
+'keys', 'pop', 'popitem', 'setdefault', 'update', 'values', 'viewitems',
+'viewkeys', 'viewvalues']
+"""
+
 source_suffix = ['.rst', '.md']
